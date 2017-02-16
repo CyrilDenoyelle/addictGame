2cea907e212d785cfe2cec740fcf4e3657378cf6
CyrilDenoyelle <Cyril-denoyelle@hotmail.fr>
2017-02-16 14:18:24 +0100

    ajout tire de seringues

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	var game = new Phaser.Game(600, 580, Phaser.AUTO, "#app", {preload: preload, create: create, update: update});
 
-	var seringue,
-	fond,
-	bullet,
+	var fond,
+	seringue,
+	bullets,
 	player;
 
 	function preload(){
@@ -16,10 +16,22 @@ $(document).ready(function(){
 
 		fond = game.add.sprite(0, 0, "fond");
 
-		var bullets = game.add.group();
+		bullets = game.add.group();
+		bullets.enableBody = true;
+
+		var seringue = bullets.create( 200, 0, "seringue");
+		seringue.body.gravity.y = 600;
+
+		player = game.add.sprite(32, game.world.height - 150, 'player');
 
 		weapon = game.add.weapon(30, 'seringue');
-		player = game.add.sprite(32, game.world.height - 150, 'player');
+		weapon.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+		weapon.bulletSpeed = 600;
+
+		weapon.trackSprite(player, 0, -400)
+
+		weapon.fireRate = 100;
+
 		cursors = game.input.keyboard.createCursorKeys();
 		game.physics.arcade.enable(player);
 
@@ -28,7 +40,9 @@ $(document).ready(function(){
 	}
 	function update(){
 
+		weapon.fire();
 
+		game.physics.arcade.overlap(player, bullets, collectStar, null, this);
 
 		player.body.velocity.x = 0;
 		if(cursors.left.isDown){
@@ -46,10 +60,10 @@ $(document).ready(function(){
 			player.frame = 4;
 		}
 
-		function collectStar(player, bullet){
-			bullet.kill();
-			score+=10;
-			scoreText.text = "Score: " + score;
+		function collectStar(player, seringue){
+			seringue.kill();
+			// // score+=10;
+			// scoreText.text = "Score: " + score;
 		}
 
 		game.world.wrap(player, 16);
